4a1815ec8f8e4b3df36e2bf7c471c3ace176a74c
Zagir2000 <zagir2324l@gmail.com>
2025-09-04 18:30:57 +0300

    Audio error

--- internal/tts/mozilla_service.go
+++ internal/tts/mozilla_service.go
@@ -71,8 +71,7 @@ func (s *MozillaService) checkMozillaTTS() error {
 	// Пробуем разные пути к TTS
 	ttsPaths := []string{
 		"tts",                                // Глобальный путь
-		"/usr/local/bin/tts",                 // Симлинк
-		"/opt/tts_env/bin/tts",               // Volume mount
+		"/opt/tts/bin/tts",                   // Volume mount
 		"/opt/lingua-ai-tts/tts_env/bin/tts", // Прямой путь к хосту
 	}
 
